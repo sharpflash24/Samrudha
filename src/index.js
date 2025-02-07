2de024a701a8270ec3d796c4d0d119beff36b5c6
smrk2415 <smarakgartia2415@gmail.com>
2025-02-06 23:48:10 -0500

    add express middleware for CORS and cookie parsing; implement error and response handling utilities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`\n Server started on PORT: ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed !!! ", err);
+})
